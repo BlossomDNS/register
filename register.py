8a5b79a3f77927359c711fb556483883d414e382
JewishLewish <lanbrokhman@gmail.com>
2023-10-21 22:13:51 -0700

    cleaned to classes

--- register.py
+++ register.py
@@ -7,42 +7,40 @@ class Cloudflare:
         self.ACCOUNT_ID = account_id
         self.ZONE_ID = zone_id
 
+        self.headers = {
+            "Authorization": f"Bearer {self.API_TOKEN}",
+            "Content-Type": "application/json"
+        }
+    
+    def insert_A_record(self, DNS_RECORD_NAME:str, DNS_RECORD_TYPE:str, DNS_RECORD_CONTENT: str, TTL:int = 1, PROXIED: bool = False):
+        dns_record_data = {
+            "type": DNS_RECORD_TYPE,
+            "name": DNS_RECORD_NAME,
+            "content": DNS_RECORD_CONTENT,
+            "ttl": TTL,
+            "proxied": PROXIED
+        }
+        self.execute(dns_record_data)
+    
+    def execute(self, dns_record_data):
+        response = requests.post(url, headers=self.headers, data=json.dumps(dns_record_data))
+
+        # Check the response
+        if response.status_code == 200:
+            print("DNS record added successfully.")
+            print("Response:", response.json())
+        else:
+            print("Failed to add DNS record.")
+            print("Status Code:", response.status_code)
+            print("Response:", response.text)
+
+        
+
 
 cloudflare = Cloudflare("your-api-token","your-account-id","your-zone-id")
 
-# Set the DNS record details
-DNS_RECORD_NAME = "example.com"
-DNS_RECORD_TYPE = "A"
-DNS_RECORD_CONTENT = "192.168.1.1"
-TTL = 1  # Time to Live in seconds
-PROXIED = False
 
-# Create the DNS record data
-dns_record_data = {
-    "type": DNS_RECORD_TYPE,
-    "name": DNS_RECORD_NAME,
-    "content": DNS_RECORD_CONTENT,
-    "ttl": TTL,
-    "proxied": PROXIED
-}
+cloudflare.insert_A_record(DNS_RECORD_NAME="example.com", DNS_RECORD_TYPE="A", DNS_RECORD_CONTENT="192.168.1.1")
 
-# Set the API endpoint URL
-url = f"https://api.cloudflare.com/client/v4/zones/{cloudflare.ZONE_ID}/dns_records"
-
-# Set headers with the API token and content type
-headers = {
-    "Authorization": f"Bearer {cloudflare.API_TOKEN}",
-    "Content-Type": "application/json"
-}
-
-# Send a POST request to add the DNS record
-response = requests.post(url, headers=headers, data=json.dumps(dns_record_data))
-
-# Check the response
-if response.status_code == 200:
-    print("DNS record added successfully.")
-    print("Response:", response.json())
-else:
-    print("Failed to add DNS record.")
-    print("Status Code:", response.status_code)
-    print("Response:", response.text)
+#TTL = 1  # Time to Live in seconds
+#PROXIED = False
