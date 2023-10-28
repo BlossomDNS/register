62b1fef4681ac049a2592d9ba6ab253478d46452
JewishLewish <lanbrokhman@gmail.com>
2023-10-27 18:07:46 -0700

    ignore github.py

--- github.py
+++ github.py
@@ -26,10 +26,10 @@ from config import GITHUB_REPO
 #         print(f"Request failed with status code {response.status_code}")
 
 
-def load_github_sites(app): #for main.py
-    @app.route('/pr')
-    def pr():
-        date_format = "%Y-%m-%dT%H:%M:%SZ"
-        pulls_data = get_pr_date()
-        links = [{"title":pull["title"], "url": pull['html_url'], "date": datetime.strptime(pull['created_at'], date_format).strftime("%Y-%m-%d"), "user":pull["user"]["login"]} for pull in pulls_data]
-        return render_template('github.html', links = links, n = len(links))
+#def load_github_sites(app): #for main.py
+#    @app.route('/pr')
+#    def pr():
+#        date_format = "%Y-%m-%dT%H:%M:%SZ"
+#        pulls_data = get_pr_date()
+#        links = [{"title":pull["title"], "url": pull['html_url'], "date": datetime.strptime(pull['created_at'], date_format).strftime("%Y-%m-%d"), "user":pull["user"]["login"]} for pull in pulls_data]
+#        return render_template('github.html', links = links, n = len(links))*/
