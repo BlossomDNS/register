d1110012afb339a498b29f219cbc376fbe29f6e2
JewishLewish <lanbrokhman@gmail.com>
2023-10-27 18:05:00 -0700

    Git undo

--- config.py
+++ config.py
@@ -4,9 +4,9 @@
 GITHUB_REPO = "BlossomDNS/register"
 GITHUB_SUBDOMAIN_JSON = f"https://raw.githubusercontent.com/{GITHUB_REPO}/main/subdomain.json"
 #cloudflare
-CLOUDFLARE_API_TOKEN = ""
-CLOUDFLARE_ACCOUNT_ID = ""
-CLOUDFLARE_ZONE_ID = ""
+CLOUDFLARE_API_TOKEN = "VnB5Y3ixxUBki4o5L9ZJqGFOE1Lx9qOgmiEY0orX"
+CLOUDFLARE_ACCOUNT_ID = "9063adba3e67bd2d65acbaf4692a50b7"
+CLOUDFLARE_ZONE_ID = "52ba56c16e9346a7f87abb9c6a42ef8e"
 #DNS Records publishing
 PROXIED_ON = False
 TTL_INT = 1
