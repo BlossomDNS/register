c38a8d4c6775e48958296446cb81958b87d610db
JewishLewish <lanbrokhman@gmail.com>
2023-12-05 20:22:18 -0800

    using variable on debug option

--- main.py
+++ main.py
@@ -342,4 +342,4 @@ if __name__ == "__main__":
             from waitress import serve
             serve(app, host="0.0.0.0", port=PORT)
         except:
-            app.run(host="0.0.0.0", port=PORT, debug=False, threaded=True)
+            app.run(host="0.0.0.0", port=PORT, debug=DEBUG_MODE, threaded=True)
